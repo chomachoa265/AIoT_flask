4dd4fe9e596469119ab411dbc8d029321ce0f413
chomachoa265 <67446926+chomachoa265@users.noreply.github.com>
2022-12-08 11:56:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
+from six.moves import urllib
+import pandas as pd
 import os
 
 app = Flask(__name__)
 
+@app.route('/fetch')
+def fetch(request):
+    timeInterval = 1000
+    data = pd.DataFrame()
+    url = API_URL + '/hw5/getData
+    data['time'] = pd.DataFrame(json.loads(urllib.request.urlopen(url).read().decode('utf-8'))['values'])
+    return data
+
+@app.route('/predict')
+def predict(request):
+    return 0
 
 @app.route('/')
 def index():
